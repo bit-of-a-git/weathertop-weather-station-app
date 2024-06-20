10d4549377a1809a88a6ad4b5532a4a1cc921a22
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2024-06-20 23:39:14 +0100

    Nearly at Release 2, still need to work on UI and CRUD

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,18 +1,24 @@
 import { stationStore } from "../models/station-store.js";
+import { accountController } from "./account-controller.js";
 
 export const dashboardController = {
   async index(request, response) {
+    const loggedInUser = await accountController.getLoggedInUser(request);
     const viewData = {
       title: "Station Dashboard",
-      stations: await stationStore.getAllStations(),
+      stations: await stationStore.getStationsByUserId(loggedInUser._id),
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
   },
 
   async addStation(request, response) {
+    const loggedInUser = await accountController.getLoggedInUser(request);
     const newStation = {
       title: request.body.title,
+      latitude: request.body.latitude,
+      longitude: request.body.longitude,
+      userid: loggedInUser._id,
     };
     console.log(`adding station ${newStation.title}`);
     await stationStore.addStation(newStation);
--- routes.js
+++ routes.js
@@ -3,10 +3,17 @@ import { homeController } from "./controllers/home-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
+import { accountController } from './controllers/account-controller.js';
 
 export const router = express.Router();
 
-router.get("/", homeController.index);
+router.get("/", accountController.index);
+router.get("/login", accountController.login);
+router.get("/signup", accountController.signup);
+router.get("/logout", accountController.logout);
+router.post("/register", accountController.register);
+router.post("/authenticate", accountController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
 router.get("/about", aboutController.index);
