595b842f34d2abf305c78b9e93815c22e955243f
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2024-06-26 23:29:01 +0100

    removed edit/update report function as it is unneeded

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
 import { accountController } from './controllers/account-controller.js';
-import { reportController } from "./controllers/report-controller.js";
 
 export const router = express.Router();
 
@@ -20,7 +19,4 @@ router.get("/delete-station/:id", dashboardController.deleteStation);
 router.get("/about", aboutController.index);
 router.get("/station/:id", stationController.index);
 router.post("/station/:id/addreport", stationController.addReport);
-
-router.get("/station/:stationid/editreport/:reportid", reportController.index);
-router.post("/station/:stationid/updatereport/:reportid", reportController.update);
 router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
